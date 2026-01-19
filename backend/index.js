646b8a61a0f29701bcd236996abc19d0a9ab2867
Naveen Dangwal <naveendangwal22@gmail.com>
2026-01-19 17:32:20 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: 'https://ticket-reservation-system-b6w7.onrender.com' || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
