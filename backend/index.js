a63b47500578bce226893fa6859967e7ef75dc09
Naveen Dangwal <naveendangwal22@gmail.com>
2026-01-19 17:37:10 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,12 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// CORS configuration
-const corsOptions = {
-    origin: 'https://ticket-reservation-system-b6w7.onrender.com' || 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-app.use(cors(corsOptions));
+app.use(cors({
+    origin:['http://localhost:5173','https://ticket-reservation-system-b6w7.onrender.com'],
+    methods:['GET','POST','PUT','DELETE'],
+    credentials:true,
+}
+));
 
 // Health check route
 app.get('/', (req, res) => {
